bac1ea0317fd3fa059d9e12b4eb13a334551ade2
pjanthony2001 <104015990+pjanthony2001@users.noreply.github.com>
2024-03-04 20:25:23 +0800

    Add JavaDoc
    Resolved errors arising from missing JavaDoc statements and also added
    most of the logic and functionality

--- src/main/java/seedu/address/history/HistoryManager.java
+++ src/main/java/seedu/address/history/HistoryManager.java
@@ -1,4 +1,65 @@
 package seedu.address.history;
 
+import seedu.address.model.Model;
+
+/**
+ * The `HistoryManager` class manages the history of commands used in the ConnectCare application.
+ * It facilitates the undoing and redoing commands and updating the command history.
+ */
 public class HistoryManager {
+    private final History history;
+
+    /**
+     * Constructs a new HistoryManager with a starting state.
+     *
+     * @param startState The initial state of the history.
+     */
+    public HistoryManager(State startState) {
+        history = new History(startState);
+    }
+
+    /**
+     * Undoes the last command and restores the previous state.
+     *
+     * @param model The model system for managing tasks.
+     * @return A message indicating the command was undone and the current list of tasks.
+     */
+    public String undo(Model model) throws Exception {
+        State prevState = history.getCurrState();
+        history.rollBackState();
+        State currState = history.getCurrState();
+        //        model.restoreState(currState);
+        // return String.format("Your %s command was undone!\nThis is your current list:\n%s",
+        //        prevState.getCommand()
+        //        model.displayList());
+        return null;
+    }
+
+    /**
+     * Redoes the last undone command and restores the next state.
+     *
+     * @param model The model system for managing tasks.
+     * @return A message indicating the command was redone and the current list of tasks.
+     */
+    public String redo(Model model) throws Exception {
+        history.rollForwardState();
+        State currState = history.getCurrState();
+        // model.restoreState(currState);
+        // return String.format("Your %s command was redone!\nThis is your current list:\n%s",
+        //      currState.getCommand(),
+        //      model.displayList());
+        return null;
+    }
+
+    /**
+     * Updates the history with a new state, if necessary.
+     *
+     * @param state The new state to be added to the history.
+     */
+    public void updateHistory(State state) {
+        //        if (state.isIgnoredHistory()) {
+        //            return;
+        //        }
+        history.addState(state);
+    }
 }
--- src/main/java/seedu/address/history/State.java
+++ src/main/java/seedu/address/history/State.java
@@ -1,4 +1,43 @@
 package seedu.address.history;
 
+import seedu.address.logic.commands.Command;
+import seedu.address.model.AddressBook;
+
+
+/**
+ * The `State` class represents a snapshot of the HAL9000 application's state at a specific point in time.
+ * It contains information about the executed command and the list of tasks at that time.
+ */
 public class State {
+    private final Command command;
+    private final AddressBook addressBook;
+
+    /**
+     * Constructs a new State object with the given command and task list.
+     *
+     * @param command  The command executed to reach this state.
+     * @param addressBook The list of tasks at this state.
+     */
+    public State(Command command, AddressBook addressBook) {
+        this.command = command;
+        this.addressBook = addressBook;
+    }
+
+    /**
+     * Gets the list of tasks at this state.
+     *
+     * @return The list of tasks.
+     */
+    public AddressBook getAddressBook() {
+        return addressBook;
+    }
+
+    /**
+     * Gets the command executed to reach this state.
+     *
+     * @return The command.
+     */
+    public Command getCommand() {
+        return command;
+    }
 }
