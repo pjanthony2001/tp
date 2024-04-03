94c1dca8e27e27c4b14ff9798f9a05e85972a18b
eliotlee68 <eliotlee68@gmail.com>
2024-04-04 01:03:43 +0800

    Fix style

--- src/test/java/seedu/address/logic/parser/CommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/CommandParserTest.java
@@ -13,7 +13,6 @@ import static seedu.address.testutil.TypicalPersons.ALICE;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
